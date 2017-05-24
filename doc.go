1b644f17bae6db0b7dff17b825845bdfd9f11ba1
Ciprian Morar <ciprian.morar@gmail.com>
2017-05-24 13:27:57 -0700

    Adding project

---
+++ doc.go
@@ -0,0 +1,6 @@
+// GoApi project doc.go
+
+/*
+GoApi document
+*/
+package main
---
+++ main.go
@@ -0,0 +1,10 @@
+// GoApi project main.go
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
